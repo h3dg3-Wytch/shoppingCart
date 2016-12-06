9040193e867bcf463fa45313e449ea7114f8e605
Rex <rexpeters3n@gmail.com>
2016-12-05 21:20:48 -0500

    Add connection to MySql Db

--- src/main/java/com/h3dg3wytch/LoginServlet.java
+++ src/main/java/com/h3dg3wytch/LoginServlet.java
@@ -14,7 +14,6 @@ import java.io.PrintWriter;
 import java.sql.*;
 import java.sql.Date;
 import java.util.*;
-import com.mysql.cj.jdbc.Driver;
 
 /**
  * Created by h3dg3wytch on 12/3/16.
@@ -28,7 +27,7 @@ public class LoginServlet extends HttpServlet {
     public static final String HTML_END="</body></html>";
 
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL="jdbc:mysql://localhost/shoppingCart";
 
     //Database creditionals
@@ -42,127 +41,67 @@ public class LoginServlet extends HttpServlet {
 
 
 
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
 
+        PrintWriter out = response.getWriter();
+        out.print(HTML_START);
         Connection connection = null;
         Statement statement = null;
 
-        PrintWriter out = response.getWriter();
-        out.print(HTML_START);
-        out.print("<h1>Login</h1>");
-        try{
-            //Step 2: register JDBC driver
-            try {
-                Class.forName(JDBC_DRIVER).newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
+        try {
+            connectionManager = new DBConnectionManager(getServletContext().getInitParameter("dbURL"),
+                    getServletContext().getInitParameter("dbUser"),
+                    getServletContext().getInitParameter("dbUserPwd"));
+            connection = connectionManager.getConnection();
+            if(connection == null)
+                out.print("<p>Null Error</p>");
 
-            //Step 3 open a connection
-            System.out.println("Connecting to database...");
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            if(connection == null){
-                out.print("<p>error null</p>");
-            }
-
-            //Exectue query
             statement = connection.createStatement();
             String sql = "SELECT * FROM user";
             ResultSet resultSet = statement.executeQuery(sql);
 
-            //Extract data from result set
-            while (resultSet.next()){
+            while(resultSet.next()){
+
                 String first = resultSet.getString("firstName");
                 String last = resultSet.getString("lastName");
-
-                out.print("<p>First: " + first +"</p>");
-                out.print("<p>Last: " + last +"</p>");
-
+                String userName = resultSet.getString("userName");
+                String password = resultSet.getString("password");
+                out.println("<p> First: " + first + "</p>");
+                out.println("<p> Last: " + last + "</p>");
+                out.println("<p> UserName: " + userName + "</p>");
+                out.println("<p> password: " + password + "</p>");
             }
 
-            resultSet.close();
-            statement.close();
-            connection.close();
+
         } catch (ClassNotFoundException e) {
+            out.print("<p>Class Error</p>");
             e.printStackTrace();
-            out.print("<p>error class2</p>");
         } catch (SQLException e) {
-            e.printStackTrace();
-            out.print("<p>error sql</p>");
+            out.print("<p>SQL Error</p>");
+            out.print("<p>" + e.getMessage() + " " + e.getSQLState() + "</p>");
         }finally {
-
-            if(statement != null){
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-            if(connection != null){
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+            try{
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
         }
+
         out.print(HTML_END);
-//
-//        Connection connection = null;
-//        Statement statement = null;
-//
-//
-//        try {
-//
-//            connectionManager = new DBConnectionManager(getServletContext().getInitParameter("dbURL"),
-//                    getServletContext().getInitParameter("dbUser"),
-//                    getServletContext().getInitParameter("dbUserPwd"));
-//            connection = connectionManager.getConnection();
-//
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        PrintWriter out = response.getWriter();
-//
-//        out.print(HTML_START);
-//        out.print("<h1>Login Page</h1>");
-//        out.print("<p>" +  getServletContext().getInitParameter("dbURL") + " " + getServletContext().getInitParameter("dbUser") + " " + getServletContext().getInitParameter("dbUserPwd") + "</p>" );
-//        if(connection == null){
-//            out.print("<p>ERROR</p>");
-//        }
-//
-//        try{
-//            statement = connection.createStatement();
-//            String sql = "SELECT * FROM user";
-//            ResultSet resultSet = statement.executeQuery(sql);
-//
-//            while(resultSet.next()){
-//
-//                String first = resultSet.getString("firstName");
-////                String last = resultSet.getString("lastName");
-////                String userName = resultSet.getString("userName");
-////                String password = resultSet.getString("password");
-//
-//                out.println("<p> First: " + first + "</p>");
-//            }
-//
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//
-//            out.println("<p>Error</p>");
-//        }
-//
-//        out.print(HTML_END);
+
+        //Make sure to close the
+
+
+
 
 
     }
