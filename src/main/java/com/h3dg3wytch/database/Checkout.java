302ad3f76318f9d6845b3e111fad12b1a8cd7188
Rex <rexpeters3n@gmail.com>
2016-12-10 16:01:21 -0500

    Add checkout post logic

--- src/main/java/com/h3dg3wytch/database/Checkout.java
+++ src/main/java/com/h3dg3wytch/database/Checkout.java
@@ -1,11 +1,31 @@
 package com.h3dg3wytch.database;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by h3dg3wytch on 12/9/16.
  */
+@WebServlet(description = "Checkout Page", urlPatterns = {"/Checkout"})
+
 public class Checkout extends HttpServlet {
 
-    
+    public static final String HTML_START="<html><body>";
+    public static final String HTML_END="</body></html>";
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println(HTML_START);
+        if(req.getParameter("checkout") != null){
+            out.print(req.getParameter("product"));
+        }
+        out.println(HTML_END);
+    }
 }
